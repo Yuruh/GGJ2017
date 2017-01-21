c7fe42e9b1fe25b334663a40be8d5000308b8f60
Hautier <thomas.hautier@epitech.eu>
2017-01-21 15:01:14 +0100

    bonne pos decalage

--- src/Map.cpp
+++ src/Map.cpp
@@ -59,8 +59,8 @@ void        Map::placeTower(std::list<ATower*> &towers, std::list<Monster*> &mon
     if (_map[y][x] == WALL)
     {
         _map[y][x] = TOWER;
-        towers.push_back(new BasicTower(x * TILE_SIZE, y * TILE_SIZE, 10, 10, 5, 5.0, nullptr, nullptr, monsters));
-        towers.back()->setPosition(y * TILE_SIZE, x * TILE_SIZE);
+        towers.push_back(new BasicTower((x - 1) * TILE_SIZE, (y - 1) * TILE_SIZE, 10, 10, 5, 5.0, nullptr, nullptr, monsters));
+        towers.back()->setPosition((y - 1) * TILE_SIZE, (x - 2) * TILE_SIZE);
         towers.back()->setProportionalSize(100, 100);
         std::cout << "place tower :" << towers.size() << std::endl;
         _towers -= 1;
