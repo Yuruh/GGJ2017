ce8b42a2df1b093626808e37fd4f5e9ba9f52ca3
Baptiste Cadoux <baptiste.cadoux@epitech.eu>
2017-01-21 11:53:26 +0100

    bug block

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -48,18 +48,23 @@ void GameEngine::handleEvent(std::pair<int, int> &event)
     if (_isLaunched) {
         std::cout << "J'utilise dans le gameEngine un clic en pos " <<
                   event.first << " " << event.second << std::endl;
+
+        if (event.first > 0 && event.first < MAP_SIZE * TILE_SIZE &&
+            event.second > 0 && event.second < MAP_SIZE * TILE_SIZE)
+        {
+            while (event.first % TILE_SIZE != 0)
+                event.first -= 1;
+            while (event.second % TILE_SIZE != 0)
+                event.second -= 1;
+            event.first /= TILE_SIZE;
+            event.second /= TILE_SIZE;
+            if (_map->getMap()[event.first][event.second] == ROAD)
+                _map->setType(event.first, event.second, BLOCK);
+        }
         return;
     }
+
     for (auto & button : _buttons)
         if (button->containMouse(event)) // Will need binding between button and behavior
             this->nextWave();
-
-    while (event.first % TILE_SIZE != 0)
-        event.first -= 1;
-    while (event.second % TILE_SIZE != 0)
-        event.second -= 1;
-    event.first /= TILE_SIZE;
-    event.second /= TILE_SIZE;
-    if (_map->getMap()[event.first][event.second] == ROAD)
-        _map->setType(event.first, event.second, BLOCK);
 }
