8b6fce1067db9d3d1375cadb07b2014c3032b836
Baptiste Cadoux <baptiste.cadoux@epitech.eu>
2017-01-22 15:05:54 +0100

    salut

--- include/Map.hpp
+++ include/Map.hpp
@@ -30,7 +30,6 @@ public:
 
     inline const std::array<std::array<Tile, MAP_X>, MAP_Y> &getMap() const { return _map; };
     inline const SfmlSpriteHandler &getGround() const { return _ground; };
-    inline const SfmlSpriteHandler &getLava() const { return _lava; };
     inline const SfmlSpriteHandler &getWall() const { return _wall; };
     inline const SfmlSpriteHandler &getBlock() const { return _block;};
     inline const SfmlSpriteHandler &getCastle() const { return _castle;};
@@ -38,7 +37,6 @@ public:
 
     inline void setPositionWall(float y, float x) { _wall.setPosition(x, y); };
     inline void setPositionGround(float y, float x) { _ground.setPosition(x, y); };
-    inline void setPositionLava(float y, float x) { _lava.setPosition(x, y); };
     inline void setPositionBlock(float y, float x) { _block.setPosition(x, y); };
     inline void setPositionCastle(float y, float x) { _castle.setPosition(x, y); };
     inline void setPositionTower(float y, float x) { _tower.setPosition(x, y); };
@@ -55,7 +53,6 @@ private:
     SfmlSpriteHandler _block;
     SfmlSpriteHandler _castle;
     SfmlSpriteHandler _tower;
-    SfmlSpriteHandler _lava;
 
 public:
 
